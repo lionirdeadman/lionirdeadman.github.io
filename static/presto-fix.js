d04d194a8d0649a5ab3334c5df7bb63ea1670c7a
Lionir <git@thelion.website>
2021-09-03 09:53:25 -0400

    Update homepage layout and layout

--- static/presto-fix.js
+++ static/presto-fix.js
@@ -1,3 +1,3 @@
 if (navigator.userAgent.match(/Opera/)) {
-  document.write('<style>.main { display: block; }</style>');
+  document.write('<style>.main { display: block; } .post-entry { margin: 20px 0; } .category-seperator { margin: 5px 0px; }</style>');
 };
